86fa9abd00b2be5ae8fa6965af92a08c394ccc1e
wayneshaw349 <wayneshaw349@hotmail.com>
2025-12-21 08:30:18 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,3 @@
 pub fn version() -> &'static str {
     "1.0.0"
 }
-
-// You can add shared logic here later that both the Frontend 
-// and the Validator nodes might need (like math or cryptography).
